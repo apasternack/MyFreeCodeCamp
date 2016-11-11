290ff1d1ed11a391c69ad7b1fe65e7af027a947e
Adam Pasternack <adam.pasternack@gmail.com>
2016-11-11 14:57:54 -0500

    DNA Pairing

--- DNAPairing.js
+++ DNAPairing.js
@@ -1,10 +1,36 @@
 
-function pairElement(str) {
-  
-  
-  
-  
-  return str;
+function pairElement(nucleotideString) {
+
+    var dnaBasePairs = [["A", "T"], ["C", "G"], ["G", "C"], ["T", "A"]];
+    var basePairToAdd;
+    var matchesBasePairs = [];
+
+    //loop through each character of the nucleotideString
+    for (var i = 0; i < nucleotideString.length; i++) {
+
+        //match up character with appropriate base pair 
+
+        findBasePair(nucleotideString[i]);
+
+        function findBasePair(nucleotide) {
+
+            basePairToAdd = dnaBasePairs.filter(
+                function (nucleotideInBasePairArray) {
+
+                    if (nucleotide === nucleotideInBasePairArray[0]) {
+                        return true;
+                    }
+                }
+            );
+            console.log(basePairToAdd);
+            basePairToAdd = basePairToAdd[0];
+            console.log(basePairToAdd);
+            matchesBasePairs.push(basePairToAdd);
+
+        }
+    }
+
+    return matchesBasePairs;
 }
 
-pairElement("GCG");
+console.log(pairElement("GCG"));
