0bf075454fb1f09b79552c3cd2155edfe1c36fea
Adam Pasternack <adam.pasternack@gmail.com>
2016-11-14 22:27:09 -0500

    Sorted Union v1

---
+++ SortedUnion.js
@@ -0,0 +1,31 @@
+
+function uniteUnique(arr) {
+
+    var uniqueValuesArray = [];
+
+    var argumentsArray = Array.prototype.slice.call(arguments);
+;
+    //loop through arrays
+
+    argumentsArray.forEach(function (arrayOfValues) {
+
+        arrayOfValues.forEach(function (inputValue) {
+
+            var addValue = true;
+
+            uniqueValuesArray.forEach(function (outputValue) {
+
+                if (outputValue === inputValue) addValue = false;
+
+            });
+
+            if (addValue = true) uniqueValuesArray.push(inputValue);
+
+        });
+
+    });
+
+    return uniqueValuesArray;
+}
+
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
