b62fd1c3ec270fc2f792207d31f90b807b8ed9f5
Adam Pasternack <adam.pasternack@gmail.com>
2016-11-29 22:08:31 -0500

    saved

---
+++ sendEmail.php
@@ -0,0 +1,65 @@
+<!--gets html form values into php variables-->
+
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+?>
+
+
+<!--composes the email-->
+<?php
+    $email_from = 'yourname@yourwebsite.com';
+ 
+    $email_subject = "New Form submission";
+ 
+    $email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message".
+?>
+
+
+<!--sending the email-->
+<?php
+ 
+  $to = "yourname@yourwebsite.com";
+ 
+  $headers = "From: $email_from \r\n";
+ 
+  $headers .= "Reply-To: $visitor_email \r\n";
+ 
+  mail($to,$email_subject,$email_body,$headers);
+ 
+ ?>
+
+ <!--protection against email injection spam attacks-->
+ <?php
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+                
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+     
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+ 
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+?>
